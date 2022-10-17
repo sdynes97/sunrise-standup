69dfb4f0f5fe91b279201f768ac6d41d774bdfc8
Scott Dynes <31550598+sdynes97@users.noreply.github.com>
2022-10-17 14:27:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 // An color-customized version of the Bulma CSS Framework
 // (https://bulma.io)
 import "./site.scss";
